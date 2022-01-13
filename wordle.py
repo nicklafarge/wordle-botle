1a6e062316324c8d1884919f1bf2808cb722f37f
Nick LaFarge <nick.lafarge@gmail.com>
2022-01-13 16:34:25 -0500

    dane's edge case handled messily

--- wordle.py
+++ wordle.py
@@ -6,7 +6,7 @@ frequency_list = {v[0]: int(v[1]) for v in frequency_list if len(v[0]) == 5 and 
 possible_words = frequency_list.keys()
 
 word = list('-----')
-
+# "resin house shoes"
 for i in range(6):
 
     if i == 0:
@@ -22,7 +22,7 @@ for i in range(6):
     if i > 0:
         freqs = [frequency_list[w] for w in possible_words[0:5]]
         freqs = [f / sum(freqs) for f in freqs]
-        fout = [f"{possible_words[i]} ({freqs[i]:.2f})" for i in range(5)]
+        fout = [f"{possible_words[i]} ({freqs[i]:.2f})" for i in range(min(5, len(possible_words)))]
         print(f'Top 5: {{{", ".join(fout)}}}')
     print(f'Guess: {guess} ({len(possible_words)} possible)')
     val = input("Wordle response (b y g): ")
@@ -30,15 +30,18 @@ for i in range(6):
     for j, ryg_val in enumerate(val):
         if ryg_val == 'g':
             possible_words = [w for w in possible_words if w[j] == guess[j]]
-            if word[j] == '-':
-                word[j] = guess[j]
+            word[j] = guess[j]
         elif ryg_val == 'y':
             possible_words = [w for w in possible_words if w[j] != guess[j] and guess[j] in w]
-        elif ryg_val == 'b':
-            if guess[j] in word: # Dane's edge case
-                possible_words = [w for w in possible_words if w[j] != guess[j] and guess[j] in w]
-            else:
-                possible_words = [w for w in possible_words if guess[j] not in w]
+
+    for j, ryg_val in enumerate(val):
+        if ryg_val != 'b':
+            continue
+
+        if guess[j] in word: # Dane's edge case
+            possible_words = [w for w in possible_words if w[j] != guess[j] and guess[j] in w]
+        else:
+            possible_words = [w for w in possible_words if guess[j] not in w]
 
     if val == 'ggggg':
         print("You did it. You successfully cheated at a stupid word game.")
